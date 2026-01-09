c88dcb34dbf653c3d346772eee30f6955d538f19
galipascali <miamit112@gmail.com>
2026-01-09 16:58:07 +0200

    fix CR

--- src/utils.ts
+++ src/utils.ts
@@ -8,9 +8,9 @@ export function assertExists(value: any, message?: string): asserts value {
 
 export const encryptPassword = async (password: string) => {
   const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
+  return bcrypt.hash(password, salt);
 };
 
 export const comparePassword = async (password: string, hash: string) => {
-  return await bcrypt.compare(password, hash);
+  return bcrypt.compare(password, hash);
 };
